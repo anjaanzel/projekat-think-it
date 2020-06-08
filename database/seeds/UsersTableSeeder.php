02b604e8cdfa5c6a122295e125b5fc2af761bfa4
anjaanzel <54682921+anjaanzel@users.noreply.github.com>
2020-06-08 23:38:48 +0200

    Update UsersTableSeeder.php

--- database/seeds/UsersTableSeeder.php
+++ database/seeds/UsersTableSeeder.php
@@ -17,12 +17,6 @@ class UsersTableSeeder extends Seeder
     {
      //User::truncate();
 
-     /**
-     $adminRole = Role::where('name', 'admin')->first();    
-     $captainRole = Role::where('name', 'captain')->first();    
-     $engineerRole = Role::where('name', 'engineer')->first();    
-     $crewRole = Role::where('name', 'crew')->first();  
-     */ 
 
      $admin = User::create([
      	'name'=>'Admin',
@@ -33,33 +27,34 @@ class UsersTableSeeder extends Seeder
           'ship_id'=>'1'
      ]);
 
-     // $captain = User::create([
-     // 	'name'=>'Petar',
-     // 	'surname'=>'Petrovic',
-     // 	'email'=>'petar@petrovic.com',
-     // 	'password'=>Hash::make('password'),
-     // 	'role_id'=>'27',
-     //      'ship_id'=>'2'
-     // ]);
+        
+     $captain = User::create([
+          'name'=>'Petar',
+          'surname'=>'Petrovic',
+          'email'=>'petar@petrovic.com',
+          'password'=>Hash::make('password'),
+          'role_id'=>'2',
+          'ship_id'=>'1'
+     ]);
 
 
-     // $engineer = User::create([
-     // 	'name'=>'Marko',
-     // 	'surname'=>'Markovic',
-     // 	'email'=>'marko@markovic.com',
-     // 	'password'=>Hash::make('password'),
-     // 	'role_id'=>'28',
-     //      'ship_id'=>'2'
-     // ]);
+     $engineer = User::create([
+          'name'=>'Marko',
+          'surname'=>'Markovic',
+          'email'=>'marko@markovic.com',
+          'password'=>Hash::make('password'),
+          'role_id'=>'3',
+          'ship_id'=>'1'
+     ]);
 
-     // $crew = User::create([
-     // 	'name'=>'Nadica',
-     // 	'surname'=>'Vidic',
-     // 	'email'=>'nadica@vidic.com',
-     // 	'password'=>Hash::make('password'),
-     // 	'role_id'=>'28',
-     //      'ship_id'=>'2'
-     // ]);
+     $crew = User::create([
+          'name'=>'Nadica',
+          'surname'=>'Vidic',
+          'email'=>'nadica@vidic.com',
+          'password'=>Hash::make('password'),
+          'role_id'=>'4',
+          'ship_id'=>'1'
+     ]);
 
 
     }
